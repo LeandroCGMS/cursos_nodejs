8e2a7fe6a88e620c2167600f092a513cbb8f1833
LeandroCGMS <leandrocgms2015@gmail.com>
2018-11-26 20:35:13 -0300

    Criando APIs com NodeJs - Aula 11: Rotas

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -9,33 +9,13 @@ const router = express.Router();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title: "Node Store API",
-        version: "0.0.2"
-    });
-});
 
-const create = router.post('/', (req, res, next) => {
-    res.status(201).send(req.body);
-});
+//Carrega as Rotas
+const indexRoute = require('./routes/index-route.js');
+const productRoute = require('./routes/product-route.js');
 
-const put = router.put('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).send({ 
-        id: id,
-        item: req.body 
-    });
-});
-
-const del = router.delete('/', (req, res, next) => {
-    res.status(200).send(req.body);
-});
-
-app.use('/', route);
-app.use('/products', create);
-app.use('/products', put);
-app.use('/products', del);
+app.use('/', indexRoute);
+app.use('/products', productRoute);
 
 
 
