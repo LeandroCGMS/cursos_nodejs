9af8d6b7bb73039a12c6c6da749c85592679bb62
LeandroCGMS <leandrocgms2015@gmail.com>
2018-12-20 15:11:21 -0300

    Criando APIs com NodeJs - Aula 27: Revisitando os Models: Order

--- node-str/src/app.js
+++ node-str/src/app.js
@@ -16,6 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // Carrega os Models
 const Product = require('./models/product');
 const Customer = require('./models/customer');
+const Order = require('./models/order');
 
 
 //Carrega as Rotas
